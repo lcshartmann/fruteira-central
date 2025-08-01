97dc784dbbb54f7880b56bc69ba13f1bcb8a5a88
lucas@vivobook <lucashartmann1337@gmail.com>
2025-08-01 14:55:42 -0300

    Initial commit

---
+++ electron/main.ts
@@ -0,0 +1,22 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true, // Enable Node.js in renderer
+      contextIsolation: false,
+    },
+  });
+
+  // Load Vite dev server in dev, static files in prod
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL('http://localhost:5173'); // Vite dev server
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
