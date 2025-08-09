829584fb1f4f9b9ded1b0a1ca85cd901da66db25
lucas@vivobook <lucashartmann1337@gmail.com>
2025-08-09 12:37:18 -0300

    Updated app configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,28 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import electron from 'vite-plugin-electron';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import electron from 'vite-plugin-electron'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
     react(),
-    electron({
-      entry: 'electron/main.ts', // Electron entry file
-    }),
+    electron([{
+      entry: 'electron/main.ts',
+     
+      vite: {
+        build: {
+          outDir: 'dist-electron',
+        },
+      },
+      
+    },{
+      entry: 'electron/preload.ts'
+    }]),
+  
   ],
-});
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
